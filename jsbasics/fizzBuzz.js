a7ac9afed7046b5d02d6f8f60fd9d98366955b7e
kyle beavin <kylebeavin@gmail.com>
2018-03-25 11:20:08 -0400

    elevator kb

--- jsbasics/fizzBuzz.js
+++ jsbasics/fizzBuzz.js
@@ -8,4 +8,51 @@
 //     }else {
 //         console.log(i);
 //     }
+// 
+// function myfunc(x,y) {
+//     let z = x + y;          ///simple function that adds two numbers
+//     console.log(z);
 // }
+
+// myfunc(1,2)
+
+// var x = "string";
+// var y = 0;
+// var z = true;
+// x=z;                       ///reassign variable and change data type
+// console.log(x);
+// console.log(typeof x)
+
+write down what you do:
+    software engineer
+    web application developer
+    UX UI specialist
+
+write a very short story that illustrates what you do 
+    right now im training at eleven fifty academy. Part of my day is spent head down coding, 
+    learning the latest technologies from mentors and teaching myself using multiple resources.
+    I am full time developing web applications.
+
+write down your objective or global
+    my goal is to find a fulfilling career that makes me happy
+
+write 5-10 action statements
+    recruited and mentored new associates
+    conducted daily weekly monthly meetings
+    merchandised and blueprinted a large showroom
+    updated key performance indicators 
+    audited all loss prevention and best practice reports
+
+elevator pitch
+    Hi, I'm Kyle beavin i'm a software developer. Right now Im actually getting certified in web 
+    development and learning javascript. 
+
+        You know I read that 2018 will be a challenging year for recruiters. Unemployments  
+        down but employers are hiring more people than they did last year. People also get comfortable
+        they stop updating their resumes. Thats why I like to keep learning new things.
+        I want to be able to add something to my resume every year.
+
+
+
+
+        
